cab95ef85a9a0b6390e50b808bf46c3eaebf8592
Maciej Bodziak <maciej.bodziak@gmail.com>
2024-04-16 18:46:59 +0200

    basic character setup, with basic animations and selection frame

---
+++ Assets/Scripts/SelectedVisual.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SelectedVisual : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer m_SpriteRenderer;
+    [SerializeField] bool isVisible;
+    public void ShowSelectedVisual()
+    {
+        isVisible = true;
+        SetSelectedVisual(true);
+    }
+
+    public void HideSelectedVisual()
+    {
+        isVisible = false;
+        SetSelectedVisual(false);
+    }
+
+    public void SetSelectedVisual(bool b)
+    {
+        isVisible = b;
+        m_SpriteRenderer.enabled = b;
+    }
+
+    public void Toggle()
+    {
+        isVisible = !isVisible;
+        SetSelectedVisual(isVisible);
+    }
+
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    Animator anim;
+    SelectedVisual selectedVisual;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        selectedVisual = GetComponent<SelectedVisual>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Attack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Hurt();
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            selectedVisual.Toggle();
+        }
+    }
+
+
+    void Attack()
+    {
+        anim.SetTrigger("Attack");
+    }
+
+    void Hurt()
+    {
+        anim.SetTrigger("Hurt");
+    }
+}
